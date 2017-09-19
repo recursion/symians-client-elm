19f88afdf28cc2b7ca35655c5498cfc41fdc39e9
Michael Symmes <msymmes@gmail.com>
2017-09-18 20:43:17 -0400

    removed old comment.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { Main } from './Main.elm';
 import loadingGif from '../public/images/loading.gif';
 require('./styles.css');
-
-// load our assets and start a service worker
 Main.fullscreen({images: {loading: loadingGif}});
